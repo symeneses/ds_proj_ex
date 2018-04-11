8e389aab9fcabcff3adf4123d2bd8218a205b04e
symeneses <sandrayojana@gmail.com>
2018-04-11 10:04:32 +0200

    Initial commit

---
+++ src/data/make_dataset.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+import os
+import click
+import logging
+import pandas as pd
+#from dotenv import find_dotenv, load_dotenv
+
+
+@click.command()
+@click.argument('input_filepath', type=click.Path(exists=True))
+@click.argument('output_filepath', type=click.Path())
+def main(input_filepath, output_filepath):
+    """ Runs data processing scripts to turn raw data from (../raw) into
+        cleaned data ready to be analyzed (saved in ../processed).
+    """
+
+    logger = logging.getLogger(__name__)
+    logger.info('making final data set from raw data')
+
+    dataset = load_data(input_filepath)
+
+def load_data(input_filepath):
+    path = os.path.join(input_filepath, 'data\\raw\\train.csv')
+    dataset = pd.read_csv(path)
+    return dataset
+
+
+
+if __name__ == '__main__':
+    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    logging.basicConfig(level=logging.INFO, format=log_fmt)
+
+    # not used in this stub but often useful for finding various files
+    project_dir = os.path.join(os.path.dirname(__file__), os.pardir, os.pardir)
+
+    # find .env automagically by walking up directories until it's found, then
+    # load up the .env entries as environment variables
+    load_dotenv(find_dotenv())
+
+    main(project_dir + '/data/raw/', project_dir + '/data/processed/')
